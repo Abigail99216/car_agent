c94dd1bf8ce93f3b0bc479815220f623e890614b
Abigail99216 <chenshuyi216@gmail.com>
2024-07-28 01:32:54 +0800

    Update car_agent.py

--- 知识库/car_agent.py
+++ 知识库/car_agent.py
@@ -1,3 +1,9 @@
+# 将ZhipuAIEmbeddings实例化移到全局范围
+embedding = ZhipuAIEmbeddings()
+
+# 将ChatZhipuAI实例化移到全局范围
+llm = ChatZhipuAI(model="glm-4", temperature=0, zhipuai_api_key="c9bc35e8e7c1c076a8aaba862efb19af.DhiaibnU9Mys34de")
+
 __import__('pysqlite3') 
 import time
 from langchain_community.chat_models import ChatZhipuAI
@@ -77,38 +83,25 @@ def get_vectordb():
     return vectordb
 
 #不带历史记录
-def get_chat_qa_chain(question:str):
+def get_chat_qa_chain(question: str):
     vectordb = get_vectordb()
-    llm = ChatZhipuAI(model = "glm-4", temperature=0, zhipuai_api_key="c9bc35e8e7c1c076a8aaba862efb19af.DhiaibnU9Mys34de")
-    memory = ConversationBufferMemory(
-        memory_key="chat_history",  # 与 prompt 的输入变量保持一致。
-        return_messages=True  # 将以消息列表的形式返回聊天记录，而不是单个字符串
-    )
-    retriever=vectordb.as_retriever()
-    qa = ConversationalRetrievalChain.from_llm(
-        llm,
-        retriever=retriever,
-        memory=memory
-    )
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    retriever = vectordb.as_retriever()
+    qa = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory)
     result = qa({"question": question})
     return result['answer']
 
 
 #带有历史记录
-def get_qa_chain(question:str):
+def get_qa_chain(question: str):
     vectordb = get_vectordb()
-    llm = ChatZhipuAI(model = "glm-4", temperature=0, zhipuai_api_key="c9bc35e8e7c1c076a8aaba862efb19af.DhiaibnU9Mys34de")
     template = """使用以下上下文来回答最后的问题。如果你不知道答案，就说你不知道，不要试图编造答
         案。最多使用三句话。尽量使答案简明扼要。总是在回答的最后说“谢谢你的提问！”。
         {context}
         问题: {question}
         """
-    QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context","question"],
-                                 template=template)
-    qa_chain = RetrievalQA.from_chain_type(llm,
-                                       retriever=vectordb.as_retriever(),
-                                       return_source_documents=True,
-                                       chain_type_kwargs={"prompt":QA_CHAIN_PROMPT})
+    QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"], template=template)
+    qa_chain = RetrievalQA.from_chain_type(llm, retriever=vectordb.as_retriever(), return_source_documents=True, chain_type_kwargs={"prompt": QA_CHAIN_PROMPT})
     result = qa_chain({"query": question})
     return result["result"]
 
